0d887797960928bfaa14bf92fbf1ce29800dcd3d
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-04-20 17:05:33 +0200

    added text bonus +++

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,12 +21,16 @@ const app = new Vue(
             change() {
                 if (this.imgUrl == "https://www.elcarteldelgaming.com/wp-content/uploads/2019/08/san.gif") {
                     this.imgUrl = "https://i.gifer.com/Nxlb.gif"
+                    this.testo = "One Punch Man Anime"
                 } else if (this.imgUrl == "https://i.gifer.com/Nxlb.gif") {
                     this.imgUrl = "https://images.squarespace-cdn.com/content/v1/5b23e822f79392038cbd486c/1630850651240-X8RLUDNCP1XRR08BDZH7/demon-slayer-anime-fight.gif"
+                    this.testo = "Demon Slayer Anime"
                 } else if (this.imgUrl == "https://images.squarespace-cdn.com/content/v1/5b23e822f79392038cbd486c/1630850651240-X8RLUDNCP1XRR08BDZH7/demon-slayer-anime-fight.gif") {
                     this.imgUrl = "https://i.ytimg.com/vi/sg1lpk4icyQ/maxresdefault.jpg"
+                    this.testo = "ju jitsu kaisen"
                 } else {
                     this.imgUrl = "https://www.elcarteldelgaming.com/wp-content/uploads/2019/08/san.gif"
+                    this.testo = "Cambiamo in modo dinamico il titolo o l'immagine"
                 }
 
 
