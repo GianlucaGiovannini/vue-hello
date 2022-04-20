27957e6360d59c214ec1ec673c0f427706b392a6
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-04-20 16:53:50 +0200

    added bonus+++

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,9 +13,26 @@ const app = new Vue(
         data: {
             testo: "Cambiamo in modo dinamico il titolo o l'immagine",
             nomeImg: "image",
-            imgUrl: "https://i0.wp.com/recommendmeanime.com/wp-content/uploads/2020/02/breath-of-fire-demon-slayer.png?fit=3840%2C2160&ssl=1",
+            imgUrl: "https://www.elcarteldelgaming.com/wp-content/uploads/2019/08/san.gif",
+
         },
 
+        methods: {
+            change() {
+                if (this.imgUrl == "https://www.elcarteldelgaming.com/wp-content/uploads/2019/08/san.gif") {
+                    this.imgUrl = "https://i.gifer.com/Nxlb.gif"
+                } else if (this.imgUrl == "https://i.gifer.com/Nxlb.gif") {
+                    this.imgUrl = "https://images.squarespace-cdn.com/content/v1/5b23e822f79392038cbd486c/1630850651240-X8RLUDNCP1XRR08BDZH7/demon-slayer-anime-fight.gif"
+                } else if (this.imgUrl == "https://images.squarespace-cdn.com/content/v1/5b23e822f79392038cbd486c/1630850651240-X8RLUDNCP1XRR08BDZH7/demon-slayer-anime-fight.gif") {
+                    this.imgUrl = "https://i.ytimg.com/vi/sg1lpk4icyQ/maxresdefault.jpg"
+                } else {
+                    this.imgUrl = "https://www.elcarteldelgaming.com/wp-content/uploads/2019/08/san.gif"
+                }
 
+
+
+            },
+
+        },
     }
 )
